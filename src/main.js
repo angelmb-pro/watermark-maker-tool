c2bf21d4c55377062a600a135b9763a8482df348
angelmb.pro <angelmb.pro@gmail.com>
2022-11-07 18:06:57 +0100

    starting decoupling input and output

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
 import ViewUserInput from './viewUserInput.js';
+import ViewOutput from './viewOutput.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const viewUserInput = new ViewUserInput();
+    const app = new App();
 });
+
+class App {
+    constructor(){
+        this.viewUserInput = new ViewUserInput();
+        this.viewOutput = new ViewOutput();
+
+        this.viewUserInput.setTestFunction(this.testFunction,this);
+        this.viewUserInput.setUploadPictureFunction(this.uploadPicture,this);
+    }
+
+    uploadPicture(imageContent){
+        console.log('upload picture!');
+        this.viewOutput.paintPicture(imageContent);
+    }
+
+    testFunction(){
+        console.log('this is a test function!');
+    }
+}
